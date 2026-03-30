e3156a892eee5866d9b5cfdd96653da1d649833b
Socramazad <marcosdp242@gmail.com>
2026-03-29 19:15:11 -0500

    se quito process.exit de supabaseclient.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,10 +4,5 @@ const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  console.error('❌ Variables faltantes:', { supabaseUrl, supabaseKey });
-  process.exit(1);
-}
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
